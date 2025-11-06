38168364106c74d44eaaa3789d7d001e4e9bff11
sergeynikishin555123123-lab <sergey.nikishin555123123@gmail.com>
2025-11-06 14:20:09 +0300

    Create vite.config.js

---
+++ client/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    host: true,
+    port: 3001,
+  },
+})
